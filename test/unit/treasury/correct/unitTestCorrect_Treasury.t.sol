f97bd3c99e09163c1c7671bdfe4b48601051f4c4
Kevin Padilla <contact@jistro.xyz>
2026-01-26 13:03:30 -0600

    Refactor: update unit correct tests for Treasury deposit and withdraw functions

---
+++ test/unit/treasury/correct/unitTestCorrect_Treasury.t.sol
@@ -0,0 +1,149 @@
+// SPDX-License-Identifier: EVVM-NONCOMMERCIAL-1.0
+// Full license terms available at: https://www.evvm.info/docs/EVVMNoncommercialLicense
+
+/**                                                                                                        
+██  ██ ▄▄  ▄▄ ▄▄ ▄▄▄▄▄▄   ▄▄▄▄▄▄ ▄▄▄▄▄  ▄▄▄▄ ▄▄▄▄▄▄ 
+██  ██ ███▄██ ██   ██       ██   ██▄▄  ███▄▄   ██   
+▀████▀ ██ ▀██ ██   ██       ██   ██▄▄▄ ▄▄██▀   ██   
+                                                    
+                                                    
+                                                    
+ ▄▄▄▄  ▄▄▄  ▄▄▄▄  ▄▄▄▄  ▄▄▄▄▄  ▄▄▄▄ ▄▄▄▄▄▄          
+██▀▀▀ ██▀██ ██▄█▄ ██▄█▄ ██▄▄  ██▀▀▀   ██            
+▀████ ▀███▀ ██ ██ ██ ██ ██▄▄▄ ▀████   ██                                                    
+ */
+
+pragma solidity ^0.8.0;
+pragma abicoder v2;
+
+import "forge-std/Test.sol";
+import "forge-std/console2.sol";
+import "test/Constants.sol";
+import "@evvm/testnet-contracts/library/Erc191TestBuilder.sol";
+import "@evvm/testnet-contracts/library/utils/AdvancedStrings.sol";
+
+contract unitTestCorrect_Treasury is Test, Constants {
+    TestERC20 testToken;
+
+    function executeBeforeSetUp() internal override {
+        evvm.setPointStaker(COMMON_USER_STAKER.Address, 0x01);
+
+        testToken = new TestERC20();
+    }
+
+    function test__unit_correct__deposit__hostNative() external {
+        vm.deal(COMMON_USER_NO_STAKER_1.Address, 0.01 ether);
+
+        vm.startPrank(COMMON_USER_NO_STAKER_1.Address);
+
+        treasury.deposit{value: 0.01 ether}(address(0), 0.01 ether);
+
+        vm.stopPrank();
+
+        assertEq(
+            evvm.getBalance(COMMON_USER_NO_STAKER_1.Address, address(0)),
+            0.01 ether,
+            "Error: incorrect balance after deposit"
+        );
+        assertEq(
+            address(treasury).balance,
+            0.01 ether,
+            "Error: incorrect treasury balance after deposit"
+        );
+        assertEq(
+            COMMON_USER_NO_STAKER_1.Address.balance,
+            0 ether,
+            "Error: incorrect user balance after deposit"
+        );
+    }
+
+    function test__unit_correct__deposit__token() external {
+        testToken.mint(COMMON_USER_NO_STAKER_1.Address, 10 ether);
+
+        vm.startPrank(COMMON_USER_NO_STAKER_1.Address);
+
+        testToken.approve(address(treasury), 10 ether);
+
+        treasury.deposit(address(testToken), 10 ether);
+
+        vm.stopPrank();
+
+        assertEq(
+            evvm.getBalance(
+                COMMON_USER_NO_STAKER_1.Address,
+                address(testToken)
+            ),
+            10 ether,
+            "Error: incorrect balance after deposit"
+        );
+        assertEq(
+            testToken.balanceOf(address(treasury)),
+            10 ether,
+            "Error: incorrect treasury token balance after deposit"
+        );
+        assertEq(
+            testToken.balanceOf(COMMON_USER_NO_STAKER_1.Address),
+            0,
+            "Error: incorrect user token balance after deposit"
+        );
+    }
+
+    function test__unit_correct__withdraw__hostNative() external {
+        vm.deal(COMMON_USER_NO_STAKER_1.Address, 0.01 ether);
+
+        vm.startPrank(COMMON_USER_NO_STAKER_1.Address);
+
+        treasury.deposit{value: 0.01 ether}(address(0), 0.01 ether);
+
+        treasury.withdraw(address(0), 0.01 ether);
+
+        vm.stopPrank();
+
+        assertEq(
+            evvm.getBalance(COMMON_USER_NO_STAKER_1.Address, address(0)),
+            0 ether,
+            "Error: incorrect balance after withdraw"
+        );
+        assertEq(
+            address(treasury).balance,
+            0 ether,
+            "Error: incorrect treasury balance after withdraw"
+        );
+        assertEq(
+            COMMON_USER_NO_STAKER_1.Address.balance,
+            0.01 ether,
+            "Error: incorrect user balance after withdraw"
+        );
+    }
+
+    function test__unit_correct__withdraw__token() external {
+        testToken.mint(COMMON_USER_NO_STAKER_1.Address, 10 ether);
+
+        vm.startPrank(COMMON_USER_NO_STAKER_1.Address);
+
+        testToken.approve(address(treasury), 10 ether);
+        treasury.deposit(address(testToken), 10 ether);
+        treasury.withdraw(address(testToken), 10 ether);
+
+        vm.stopPrank();
+
+        assertEq(
+            evvm.getBalance(
+                COMMON_USER_NO_STAKER_1.Address,
+                address(testToken)
+            ),
+            0,
+            "Error: incorrect balance after withdraw"
+        );
+        assertEq(
+            testToken.balanceOf(address(treasury)),
+            0,
+            "Error: incorrect treasury token balance after withdraw"
+        );
+        assertEq(
+            testToken.balanceOf(COMMON_USER_NO_STAKER_1.Address),
+            10 ether,
+            "Error: incorrect user token balance after withdraw"
+        );
+    }
+}
